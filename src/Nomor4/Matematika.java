655077c3e145d906fb299fcbb132de85758a2a55
putridriyani <114595104+putridriyani@users.noreply.github.com>
2023-05-04 20:56:24 +0700

    Update Matematika.java

--- src/Nomor4/Matematika.java
+++ src/Nomor4/Matematika.java
@@ -2,16 +2,41 @@ package Nomor4;
 
 public class Matematika {
     private double x;
+
     public Matematika(double x) {
+
         this.x = x;
     }
+
     public double hitungLimitsin() {
+
         return Math.sin(x) / x;
     }
+
     public double hitungLimitcos() {
+
         return Math.cos(x) / x;
     }
+
     public double hitungLimittan() {
+
         return Math.tan(x) / x;
     }
+
+    public double hitungLimitsin(double x) {
+
+        return Math.sin(x) / x;
+    }
+
+    public double hitungLimitcos(double x) {
+
+        return Math.cos(x) / x;
+    }
+
+    public double hitungLimittan(double x) {
+
+        return Math.tan(x) / x;
+    }
+
 }
+
