ed621bb4d4a3260f59e2cc63aeaa6bc5fda0b453
putridriyani <114595104+putridriyani@users.noreply.github.com>
2023-05-04 20:50:58 +0700

    Update Fisika.java

--- src/Nomor4/Fisika.java
+++ src/Nomor4/Fisika.java
@@ -4,24 +4,37 @@ public class Fisika {
     private double v;
     private double m;
     private double h;
-    private double K;
-    private double U;
+    
 
     public Fisika(double m, double v, double h) {
         this.m = m;
         this.v = v;
         this.h = h;
     }
-    public void hitungKinetik() {
-        K = 0.5 * m * Math.pow(v,2);
-        System.out.print("\nEnergi Kinetik   : 0.5 x m x v^2 = "+ K + " Joule");
+    public double hitungKinetik() {
+        return 0.5 * m * Math.pow(v,2);
+
     }
-    public void hitungPotensial() {
-        U = m * 9.8 * h;
-        System.out.print("\nEnergi Potensial : m x g x h     = "+ U + " Joule");
+    public double hitungPotensial() {
+        return  m * 9.8 * h;
     }
-    public double hitungTotal(){
-        return K + U;
+
+    public Fisika(){
+        this(25, 56, 100);
+    }
+    public void hitungTotal(){
+        System.out.print("\nEnergi Kinetik   : 0.5 x m x v^2 = "+ hitungKinetik() + " Joule");
+        System.out.print("\nEnergi Potensial : m x g x h     = "+ hitungPotensial() + " Joule");
+        System.out.print("\nEnergi Total     : K + U         = " + (hitungKinetik() + hitungPotensial()) + " Joule");
+    }
+
+    public void hitungTotal(double m, double v, double h){
+        System.out.println("\n\nMassa Objek (kg)                                : "+ m);
+        System.out.println("Kecepatan Objek (m/s)                           : "+ v);
+        System.out.println("Ketingggian Objek dari atas permukaaan tanah (m): "+ h);
+        System.out.print("\nEnergi Kinetik   : 0.5 x m x v^2 = "+ hitungKinetik() + " Joule");
+        System.out.print("\nEnergi Potensial : m x g x h     = "+ hitungPotensial() + " Joule");
+        System.out.print("\nEnergi Total     : K + U         = " + (hitungKinetik() + hitungPotensial()) + " Joule");
     }
 
 }
